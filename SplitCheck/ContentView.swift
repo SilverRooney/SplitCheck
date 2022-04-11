9654ee61f52d38a63e6c713de32c8922b2c49fe4
SilverRooney <silver.rooney.dev@gmail.com>
2022-04-10 21:49:50 -0500

    Initial Commit

---
+++ SplitCheck/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SplitCheck
+//
+//  Created by Silver on 4/10/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SplitCheck/SplitCheckApp.swift
@@ -0,0 +1,17 @@
+//
+//  SplitCheckApp.swift
+//  SplitCheck
+//
+//  Created by Silver on 4/10/22.
+//
+
+import SwiftUI
+
+@main
+struct SplitCheckApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
